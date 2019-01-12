c79f3c4191b615c772afebbc70f3b78ed18bebe0
samlin233 <senlin@umail.ucsb.edu>
2019-01-12 22:45:50 +0800

    #

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
- * Generated class for the FriendsPage page.
+ * Generated class for the MyInfoPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -18,8 +19,11 @@ export class FriendsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  public gotoHomePage() {
+    this.navCtrl.push(HomePage);
+  }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad FriendsPage');
   }
-
 }
